cd1d589e309b12eee9c3085288d3c66a3f49627c
Prakhar raj Gupta <67421657+1834813032@users.noreply.github.com>
2024-01-08 11:51:40 +0530

    Add files via upload

---
+++ employee/src/main/java/com/greatlearning/employee/data/Employee.java
@@ -0,0 +1,34 @@
+package com.greatlearning.employee.data;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Column;
+import lombok.Setter;
+import lombok.Getter;
+@Entity
+@Table (name="employees")
+@Getter
+@Setter
+public class Employee {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="id")
+	private int id;
+	@Column(name= "first_name")
+	private String firstname;
+	@Column(name= "last_name")
+	private String latname;
+	@Column(name= "email")
+	private String email;
+	
+	
+	
+	
+		
+	}
+	
+
+
