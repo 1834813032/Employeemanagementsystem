1cf214267f1b17cc101f46f418e549a3bcdb4e49
Prakhar raj Gupta <67421657+1834813032@users.noreply.github.com>
2024-01-09 14:36:35 +0530

    Delete employee directory

--- employee/src/main/java/com/greatlearning/employee/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.greatlearning.employee.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import com.greatlearning.employee.data.Employee;
-
-@Repository
-public interface EmployeeRepository extends JpaRepository<Employee, Integer>{
-
-}
